19f7e67bcd8b28222d59bfe1ad321d9100859045
ken <ken789030@gmail.com>
2021-10-13 16:06:16 +0800

    簡易網站完成

--- main.go
+++ main.go
@@ -13,17 +13,23 @@ type IndexData struct {
 
 func test(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("./index.html"))
-	// w.WriteHeader(http.StatusOK)
-	str := `<!DOCTYPE html>
-<html>
-<head><title>首頁</title></head>
-<body><h1>首頁</h1><p>我的第一個首頁</p></body>
-</html>`
-	w.Write([]byte(str))
+	data := new(IndexData)
+	data.Title = "首頁"
+	data.Content = "我的第一個首頁"
+	tmpl.Execute(w, data)
+}
+
+func test2(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./index.html"))
+	data := new(IndexData)
+	data.Title = "首頁"
+	data.Content = "我的第二個首頁"
+	tmpl.Execute(w, data)
 }
 
 func main() {
 	http.HandleFunc("/", test)
+	http.HandleFunc("/index", test2)
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
