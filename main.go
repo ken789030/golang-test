8fa8f6187dda01ca1d80e8bf6d8296d13f6a708e
ken <ken789030@gmail.com>
2022-11-09 15:36:38 +0800

    暫存

--- main.go
+++ main.go
@@ -1,13 +1,57 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"errors"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type IndexData struct {
-	Title	string
-	Content	string
+	Title   string
+	Content string
+}
+
+func LoginPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", nil)
+}
+
+func LoginAuth(c *gin.Context) {
+	var (
+		username string
+		password string
+	)
+
+	if in, isExist := c.GetPostForm("username"); isExist && in != "" {
+		username = in
+	} else {
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+			"error": errors.New("必須輸入使用者名稱"),
+		})
+		return
+	}
+
+	if in, isExist := c.GetPostForm("password"); isExist && in != "" {
+		password = in
+	} else {
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+			"error": errors.New("必須輸入密碼名稱"),
+		})
+		return
+	}
+
+	if err := Auth(username, password); err == nil {
+		c.HTML(http.StatusOK, "login.html", gin.H{
+			"success": "登入成功",
+		})
+		return
+	} else {
+		c.HTML(http.StatusUnauthorized, "login.html", gin.H{
+			"error": err,
+		})
+		return
+	}
+
 }
 
 func test(c *gin.Context) {
@@ -17,10 +61,13 @@ func test(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", data)
 }
 
-
 func main() {
 	server := gin.Default()
-	server.LoadHTMLGlob("template/*")
+	server.LoadHTMLGlob("template/html/*")
+	// 設定靜態資源的讀取
+	server.Static("/assets", "./template/assets")
 	server.GET("/", test)
+	server.GET("/login", LoginPage)
+	server.POST("/login", LoginAuth)
 	server.Run(":8888")
 }
